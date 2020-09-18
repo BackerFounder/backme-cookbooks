1e9de4ba17ad7e15e971fb724086f10a74093169
fumitsuki <fumitsuki@5xruby.tw>
2020-09-18 18:37:14 +0800

    chore: raise exception when app_source type is not `git`

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -4,6 +4,10 @@
 #
 # Copyright:: 2020, BackerFounder, All Rights Reserved.
 
+app = search(:aws_opsworks_app).first
+
+raise "Unsupported app_source type: #{app['app_source']['type']}" unless app['app_source']['type'] == 'git'
+
 include_recipe 'deploy::add_known_host'
 include_recipe 'deploy::add_rsa_keys'
 include_recipe 'deploy::sync_repo'
