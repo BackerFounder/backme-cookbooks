94a8aa39f5b767975ff18aecc58ef72d6733be24
fumitsuki <fumitsuki@5xruby.tw>
2020-11-06 16:52:59 +0800

    chore: install packages which are needed by gems

---
+++ deploy/recipes/setup.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook:: deploy
+# Recipe:: setup
+#
+# Copyright:: 2020, BackerFounder, All Rights Reserved.
+
+# resync package index
+apt_update 'update'
+
+# gem `curb` uses libcurl
+apt_package 'libcurl4-gnutls-dev'
+
+# gem `pg` uses libpq
+apt_package 'libpq-dev'
+
+# gem `ovirt-engine-sdk` uses libxml2
+apt_package 'libxml2-dev'
