52cd4a3c4940b70a15a9de5d80e843674f7bf514
fumitsuki <fumitsuki@5xruby.tw>
2020-11-11 16:38:58 +0800

    chore: pass ruby, bundle & gem path with node.run_state

--- ruby/recipes/default.rb
+++ ruby/recipes/default.rb
@@ -7,3 +7,12 @@
 rbenv_system_install ''
 rbenv_ruby node['ruby']['version']
 rbenv_global node['ruby']['version']
+
+include_recipe 'ruby::pass_vars'
+
+ruby_block 'Check Ruby version' do
+  block do
+    command = "#{node.run_state[:ruby_path]} -v"
+    Chef::Log.info("Ruby version is #{shell_out(command).stdout}")
+  end
+end
