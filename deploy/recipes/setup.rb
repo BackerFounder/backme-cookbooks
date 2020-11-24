5367fde53aa394cf001a1d0799f52ce799cb4c3d
fumitsuki <fumitsuki@5xruby.tw>
2020-11-24 12:37:55 +0800

    tmp: prevent apt_update and apt_package executing at the same time

--- deploy/recipes/setup.rb
+++ deploy/recipes/setup.rb
@@ -5,10 +5,14 @@
 # Copyright:: 2020, BackerFounder, All Rights Reserved.
 
 # resync package index
-apt_update 'update'
+apt_update 'update' do
+  action :nothing
+end
 
 # gem `curb` uses libcurl
-apt_package 'libcurl4-gnutls-dev'
+apt_package 'libcurl4-gnutls-dev' do
+  notifies :update, 'apt_update[update]', :before
+end
 
 # gem `pg` uses libpq
 apt_package 'libpq-dev'
