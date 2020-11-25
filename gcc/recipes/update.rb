e63e00937700206e83aeba6284e0bdac7053d08f
fumitsuki <fumitsuki@5xruby.tw>
2020-11-25 12:06:13 +0800

    fix: call next step with `notifies`

--- gcc/recipes/update.rb
+++ gcc/recipes/update.rb
@@ -2,18 +2,24 @@
 execute 'Add ubuntu toolchain to apt repo' do
   user 'root'
   command 'add-apt-repository -y ppa:ubuntu-toolchain-r/test'
+  notifies :run, 'execute[apt-get update]', :immediately
 end
 
 execute 'apt-get update' do
   command 'apt-get update'
+  notifies :run, 'execute[install gcc]', :immediately
+  action :nothing
 end
 
 execute 'install gcc' do
   user 'root'
   command 'apt-get install gcc g++ gcc-7 g++-7'
+  notifies :run, 'execute[update-alternatives]', :immediately
+  action :nothing
 end
 
 execute 'update-alternatives' do
   user 'root'
   command 'update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 70 --slave /usr/bin/g++ g++ /usr/bin/g++-7'
+  action :nothing
 end
