44bf1263770911cdd4bab12daed5bc6b35c19065
fumitsuki <fumitsuki@5xruby.tw>
2021-02-22 10:20:08 +0800

    feat: configure `client_max_body_size` with chef attribute in nginx cookbook

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -2,6 +2,7 @@
 
 default['nginx']['worker_connections'] = '1024'
 default['nginx']['keepalive_timeout'] = 65
+default['nginx']['client_max_body_size'] = '2M'
 default['nginx']['log_dir'] = '/var/log/nginx'
 default['nginx']['gzip_types'] = [
   "application/json",
